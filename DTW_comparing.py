e3e69088baaba1ae9bec82d374bd94ef76700a46
MShapira <mshapira2016@gmail.com>
2018-03-15 20:25:45 +0300

    json converting

--- DTW_comparing.py
+++ DTW_comparing.py
@@ -4,8 +4,20 @@ import time
 from numpy.linalg import norm
 import gc
 from os import makedirs, path
-from multiprocessing import Process
+import multiprocessing as m
 import json
+import threading
+
+
+class WorkerThread(m.Process):
+    def __init__(self, queue, size):
+        m.Process.__init__(self)
+        self.queue = queue
+        self.size = size
+
+    def run(self):
+        self.queue.put(range(self.size))
+
 
 start_time = time.time()
 
@@ -18,13 +30,19 @@ def dtw_pare_comparing(pare):
     data = json.loads(pare)
 
     with open('results/' + data['key']['name'] + '-' + data['aa']['name'], 'w') as file:
+        print('Begin with ' + data['key']['name'] + ' and ' + data['aa']['name'])
         x = np.array([int(round(float(v), 4) * 10000) for v in data['key']['value']]).reshape(-1, 1)
+        print(data['key']['name'] + ' array is done')
         y = np.array([int(round(float(v), 4) * 10000) for v in data['aa']['value']]).reshape(-1, 1)
+        print(data['aa  ']['name'] + ' array is done')
         dist = dtw(x, y, dist=lambda x, y: norm(x - y, ord=1))[0]
         gc.collect()
 
         file.write(str(dist/10000))
         file.close()
+
+        print('Data was wroten down for ' + data['key']['name'] + '-' + data['aa']['name'])
+        print('-' * 50)
 
 
 with open('real_rmsd.txt', 'r') as file:
@@ -62,6 +80,8 @@ with open('real_rmsd.txt', 'r') as file:
 
     # calculate and collect the data using dtw lib
     final_table = {}
+    queue = m.Queue()
+
     for key in rmsd.keys():
         final_table[key] = {}
         for aa in rmsd.keys():
@@ -69,8 +89,11 @@ with open('real_rmsd.txt', 'r') as file:
                 pare = {"key": {"name": key, "value": rmsd[key]}, "aa": {"name": aa, "value": rmsd[aa]}}
                 pare = json.dumps(pare, sort_keys=True, indent=4)
 
-                results = Process(target=dtw_pare_comparing, args=(pare, ))
-                results.start()
+                queue.put(m.Process(target=dtw_pare_comparing, args=(pare, )))
+
+    worker_p = WorkerThread(queue, 30)
+    worker_p.start()
+    worker_p.join()
 
     print("--- %s seconds ---" % (time.time() - start_time))
 
